3564f351c471f116f4057491588388cf34e20a25
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-26 01:43:51 -0500

    🧼

--- Command/src/Button.ts
+++ Command/src/Button.ts
@@ -1,12 +1,9 @@
-// 2.) import the Command interface
 import { Command } from "./Command";
 
 class Button {
   private _label: string;
-  // 3.) Add a Command property to the Button class
   private _command: Command;
 
-  // 4.) Update the constructor to accept a Command parameter
   constructor(label: string, command: Command) {
     this._label = label;
     this._command = command;
@@ -20,7 +17,6 @@ class Button {
   }
 
   public click(): void {
-    // 5.) Call the execute method of the Command property so that the command is executed when the button is clicked
     this._command.execute();
   }
 }
