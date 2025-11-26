c708073abbbf05c7e025b4f68b5772ec1917ced2
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-26 03:37:27 -0500

    🧼

--- Command/src/index.ts
+++ Command/src/index.ts
@@ -4,11 +4,8 @@ import { Button } from "./fx/Button";
 
 class main {
   main(): void {
-    // 2.) create the command object
     const command = new AddCustomerCommand();
-    // 3.) create the button and assign the command to it
     const button = new Button("Add Customer", command);
-    // 5.) simulate a button click
     button.click();
   }
 }
