e7d119aa14399c0a214011d59a584ca1f089d88c
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-12-01 15:29:54 -0500

    🧼

--- Observer/src/DataSource.ts
+++ Observer/src/DataSource.ts
@@ -1,11 +1,9 @@
 import { Subject } from "./Subject";
 
-// 12.) Extend the DataSource class to inherit from Subject
 export class DataSource extends Subject {
   private _value: number;
 
   constructor(data: number) {
-    // 13.) Call the super class constructor
     super();
     this._value = data;
   }
@@ -15,7 +13,6 @@ export class DataSource extends Subject {
   }
   public set data(value: number) {
     this._value = value;
-    // 13.) Call notifyObservers method whenever data changes
     this.notifyObservers();
   }
 }
--- Observer/src/index.ts
+++ Observer/src/index.ts
@@ -4,19 +4,15 @@ import { SpreadSheet } from "./SpreadSheet";
 
 class Main {
   static main(): void {
-    // 1.) Grab a instance of the DataSource
     const dataSource = new DataSource(0);
-    // 2.) Create a new SpreadSheet and Chart observer
     const sheet1 = new SpreadSheet();
     const sheet2 = new SpreadSheet();
     const chart = new Chart();
 
-    // 3.) Tell the data source to register these objects as observers
     dataSource.addObserver(sheet1);
     dataSource.addObserver(sheet2);
     dataSource.addObserver(chart);
 
-    // 4.) Change the data in the data source
     dataSource.data = 10;
   }
 }
