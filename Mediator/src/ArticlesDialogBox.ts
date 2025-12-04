6d55c569efd7cb2fcb9de45a536c17fdfcba2186
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-12-04 16:37:45 -0500

    🧠 mediator: Demonstrate Mediator pattern usage

--- Mediator/src/ArticlesDialogBox.ts
+++ Mediator/src/ArticlesDialogBox.ts
@@ -8,6 +8,20 @@ export class ArticlesDialogBox extends DialogBox {
   private titleTextBox = new TextBox(this);
   private saveButton = new Button(this);
 
+  //   2.) Simulate user interactions in the constructor of ArticlesDialogBox.
+  public simulateUserInteraction() {
+    // 3.) Simulate the user selecting an article in the ListBox.
+    this.articlesListBox.setSelection("Article 1");
+    // 7.) Simulate the user clearing the TextBox.
+    this.titleTextBox.setContent("");
+    // 8.) Simulate the user entering a new title in the TextBox.
+    this.titleTextBox.setContent("New Article Title");
+    // 4.) Display the state of the TextBox
+    console.log("TextBox: " + this.titleTextBox.getContent());
+    // 5.) See if the Save button is enabled or not.
+    console.log("Button: " + this.saveButton.getIsEnabled());
+  }
+
   changed(control: Button | ListBox | TextBox): void {
     if (control === this.articlesListBox) this.articleSelected();
     else if (control === this.titleTextBox) this.titleChanged();
--- Mediator/src/index.ts
+++ Mediator/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, TypeScript! 🚀");
+import { ArticlesDialogBox } from "./ArticlesDialogBox";
+
+class Main {
+  public static main(): void {
+    // 1.) Create an instance of ArticlesDialogBox.
+    const dialogBox = new ArticlesDialogBox();
+    // 6.) Call the simulateUserInteraction() method to see how the mediator coordinates the interaction between the UI controls.
+    dialogBox.simulateUserInteraction();
+  }
+}
+
+Main.main();
