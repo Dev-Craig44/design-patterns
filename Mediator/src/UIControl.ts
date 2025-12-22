06b4a1ae2ea92bf401aa033ca96ea31363191696
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-12-21 19:20:50 -0500

    🧼

--- Mediator/src/UIControl.ts
+++ Mediator/src/UIControl.ts
@@ -1,12 +1,8 @@
 import { DialogBox } from "./DialogBox";
 
 export class UIControl {
-  // 6.) Add a protected getter for the owner property so that derived classes can access the dialog box mediator.
-
-  // 3.) Add a reference to the dialog box so that the control can know it's dialog box mediator.
   protected owner: DialogBox;
 
-  // 4.) Create a constructor that takes a DialogBox parameter to initialize the owner property.
   constructor(owner: DialogBox) {
     this.owner = owner;
   }
--- Mediator/src/index.ts
+++ Mediator/src/index.ts
@@ -2,9 +2,7 @@ import { ArticlesDialogBox } from "./ArticlesDialogBox";
 
 class Main {
   public static main(): void {
-    // 1.) Create an instance of ArticlesDialogBox.
     const dialogBox = new ArticlesDialogBox();
-    // 6.) Call the simulateUserInteraction() method to see how the mediator coordinates the interaction between the UI controls.
     dialogBox.simulateUserInteraction();
   }
 }
